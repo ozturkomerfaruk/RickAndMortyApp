37c1bfbe350dd779a49494c661bff037206c000f
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2023-01-24 23:41:54 +0300

    DataModels

---
+++ TheRickAndMorty/TheRickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,14 @@
+//
+//  RMOrigin.swift
+//  TheRickAndMorty
+//
+//  Created by Ömer Faruk Öztürk on 24.01.2023.
+//
+
+import Foundation
+
+struct RMORigin {
+    let name: String
+    let url: String
+
+}
